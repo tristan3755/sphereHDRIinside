91ab4962cc4ec56d1c3b630435391734e2789ed2
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-12-11 12:03:32 +0100

    ca a de la gueule

--- main.js
+++ main.js
@@ -25,8 +25,6 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 /*renderer.autoClear = false;*/
 
 
-/*camera.position.set(0,0,500)*/
-
 /*const lumiere=new THREE.PointLight(0xffffff,0.01)
 lumiere.position.set(550,0,550)
 scene.add(lumiere)*/
@@ -34,29 +32,28 @@ scene.add(lumiere)*/
 /*construction sphere*/
 
 let envmaploader= new THREE.PMREMGenerator(renderer)
-
-new RGBELoader().setPath('./').load('abstract.hdr',(hdrmap)=>{
+new RGBELoader().setPath('./').load('custom4.hdr',(hdrmap)=>{
 
   let envmap=envmaploader.fromCubemap(hdrmap)
   let textures=new THREE.CanvasTexture(new FlakesTexture())
   textures.wrapS= THREE.RepeatWrapping
   textures.wrapT= THREE.RepeatWrapping
-  textures.repeat.x=200
-  textures.repeat.y=200
+  textures.repeat.x=0
+  textures.repeat.y=0
   
   const sphereMaterials={
     clearcoat: 1,
-    clearcoatRoughness:1,
+    clearcoatRoughness:0.4,
     metalness:1,
-    roughness:1,
-    color:0x1e272e,
+    roughness:0.4,
+    /*color:0x1e272e,*/
     normalMap:textures,
-    normalScale:new THREE.Vector2(0.15,0.15),
+    normalScale:new THREE.Vector2(0.85,0.85),
     envMap:envmap.texture,
     transparent: true,
   }
   
-  let sphereConstruct=new THREE.SphereGeometry(305,200,200)
+  let sphereConstruct=new THREE.SphereGeometry(55,55,55)
   let sphereMaterial=new THREE.MeshPhysicalMaterial(sphereMaterials)
   let sphere= new THREE.Mesh(sphereConstruct,sphereMaterial)
   scene.add(sphere)
@@ -64,7 +61,7 @@ new RGBELoader().setPath('./').load('abstract.hdr',(hdrmap)=>{
   let orbit
 
 document.addEventListener('mousemove',(e)=>{
-let scale = -0.005
+let scale = -0.003
 orbit.rotateY( e.movementX * scale );
 orbit.rotateX( e.movementY * scale ); 
 orbit.rotation.z = 0;
@@ -75,27 +72,13 @@ orbit.rotation.order = "YXZ";
 orbit.position.copy( sphere.position );
 scene.add(orbit );
 
-let cameraDistance = 420;
+let cameraDistance = 70;
 camera.position.z = cameraDistance;
 orbit.add( camera );
 
 })
 
-/*construction sphere*/
-/*const controls=new OrbitControls(camera,renderer.domElement)
-
-controls.autoRotate=false
-controls.autoRotateSpeed=1
-controls.enableDamping=true*/
-/*controls.enableZoom=false*/
-
-
-
-
 function animate(){
-   
-  
-  /*controls.update()*/
 
   renderer.render(scene,camera)
   requestAnimationFrame(animate)
